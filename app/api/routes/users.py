28f03984a57f6699175f69efc7e46a963cdc1d92
Mariana Amy <mmarianaamy@gmail.com>
2025-03-27 20:51:09 -0600

    Get Employees and Clients

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -12,3 +12,21 @@ async def get_all(supabase: Client = Depends(get_supabase)):
         return response.data
     except:
         print("Error getting client")
+
+@router.get("/employees")
+async def get_all(supabase: Client = Depends(get_supabase)):
+    try:
+        response = supabase.table("users").select("user_id, username").neq("role", "client").execute()
+        users = [i["username"] for i in response.data]
+        return {"data": response.data, "users": users}
+    except:
+        print("Error getting client")
+
+@router.get("/client")
+async def get_all(supabase: Client = Depends(get_supabase)):
+    try:
+        response = supabase.table("users").select("user_id, username").eq("role", "client").execute()
+        users = [i["username"] for i in response.data]
+        return {"data": response.data, "users": users}
+    except:
+        print("Error getting client")
