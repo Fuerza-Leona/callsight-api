69fd3ead03087b839468fc6a3b14c4cb1c453317
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2025-04-22 16:18:57 -0600

    feat: continue conversations and database storage

--- app/api/routes/chatbot.py
+++ app/api/routes/chatbot.py
@@ -17,7 +17,6 @@ client = OpenAI(api_key=settings.OPENAI_API_KEY)
 class ChatRequest(BaseModel):
     prompt: str
 
-
 @router.post("/chat")
 async def post_chat(
     request: ChatRequest,
@@ -71,17 +70,16 @@ async def post_chat(
         supabase.table("chatbot_messages").insert(user_message).execute()
         supabase.table("chatbot_messages").insert(chatbot_message).execute()
         
-        #return {"response": response.output_text}
-        #return {"response": response.output_text, "title": title.output_text}
-        return {"response": response}
+        #return {"response": response}
+        return {"response": response.output_text, "title": title.output_text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/continue/{conversation_id}")
 async def continue_chat(
-    conversation_id: str,
     request: ChatRequest,
-    #current_user=Depends(get_current_user),
+    conversation_id: str,
+    current_user=Depends(get_current_user),
     supabase: Client = Depends(get_supabase)
 ):
     """
@@ -90,23 +88,39 @@ async def continue_chat(
     Test id: resp_6807cc6371d8819185c5832054ff4c910413c6fd1ef76301
     """
     try:
+        #select last response from conversation where conversation = conversation_id
+        previous_response_id = (supabase.table("chatbot_conversations").select("last_response_id").eq("chatbot_conversation_id", conversation_id).execute()).data[0]
+        previous_response_id = previous_response_id["last_response_id"]
         response = client.responses.create(
             model="gpt-3.5-turbo",
-            previous_response_id=conversation_id,
+            previous_response_id=previous_response_id,
             input=[
                 {"role": "system", "content": "You are a helpful assistant for a call center."},
                 {"role": "user", "content": request.prompt},
             ],
         )
+        created_at = datetime.fromtimestamp(response.created_at).isoformat()
+        user_message = {
+            "chatbot_message_id": str(uuid4()),
+            "chatbot_conversation_id": conversation_id,
+            "role": 'user',
+            "content": request.prompt,
+            "created_at": created_at,
+            "previous_response_id": previous_response_id
+        }
         chatbot_message = {
-            "message_id": response.id,
-            "conversation_id": conversation_id, #se la tengo que dar al endpoint
-            "role": response.output.role,
+            "chatbot_message_id": response.id,
+            "chatbot_conversation_id": conversation_id,
+            "role": 'assistant',
             "content": response.output_text,
-            "created_at": response.created_at,
+            "created_at": created_at,
             "previous_response_id": previous_response_id
         }
 
+        #modify the table to change the last message
+        supabase.table("chatbot_conversations").update({"last_response_id": response.id}).eq("chatbot_conversation_id", conversation_id).execute() #modify the table to change the last message
+        
+        supabase.table("chatbot_messages").insert(user_message).execute()
         supabase.table("chatbot_messages").insert(chatbot_message).execute()
         return {"response": response.output_text}
     except Exception as e:
