6adc030996921ec8844fe00c253fdeb3e29020e2
miguelmendozajr <miguelmendozaj20@gmail.com>
2025-04-24 11:21:54 -0600

    fix: search company participants by company name

--- app/api/routes/companies.py
+++ app/api/routes/companies.py
@@ -16,14 +16,25 @@ async def get_all_companies(supabase: Client = Depends(get_supabase)):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/{company_id}/list")
-async def get_users_in_company(
-    company_id: str, supabase: Client = Depends(get_supabase)
-):
+@router.get("/{name}/list")
+async def get_users_in_company(name: str, supabase: Client = Depends(get_supabase)):
     try:
-        response = (
+        company_response = (
+            supabase.table("company_client")
+            .select("comany_id")
+            .eq("name", name)
+            .execute()
+        )
+
+        if not company_response.data:
+            raise HTTPException(status_code=404, detail="Company not found")
+
+        company_id = company_response.data[0]["id"]
+
+        users_response = (
             supabase.table("users").select("*").eq("company_id", company_id).execute()
         )
-        return {"participants": response.data}
+
+        return {"participants": users_response.data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
