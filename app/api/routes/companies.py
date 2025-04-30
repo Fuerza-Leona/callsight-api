9076fb817e0a97dc8357dfa154cdb0fd08fbfa82
Mariana Amy <mmarianaamy@gmail.com>
2025-04-30 16:51:13 -0600

    feat: companies and sizes

--- app/api/routes/companies.py
+++ app/api/routes/companies.py
@@ -14,6 +14,22 @@ async def get_all_companies(supabase: Client = Depends(get_supabase)):
         return {"companies": response.data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/companySize")
+async def get_company_size(supabase: Client = Depends(get_supabase)):
+    try:
+        response = supabase.table("company_client").select("*, users(count)").execute()
+        dict_response = []
+        for company in response.data:
+            dict_response.append(
+                {
+                    "name": company["name"],
+                    "size": company["users"][0]["count"] if company["users"] else 0,
+                }
+            )
+        return {"info": dict_response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/{name}/list")
