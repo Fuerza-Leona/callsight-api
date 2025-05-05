1ee6b0d3742f110db1194d5cda4252360cb48a58
Mariana Amy <mmarianaamy@gmail.com>
2025-05-04 18:03:59 -0600

    feat: test companies full

--- tests/test_companies.py
+++ tests/test_companies.py
@@ -64,9 +64,37 @@ def test_company_size(mock_supabase):
     assert len(response.json()["info"]) == 1
     assert response.json()["info"][0] == {"name": "Test Company", "size": 5}
 
+
 def test_company_size_supabase_error(mock_supabase):
     mock_supabase.table("company_client").select("*, users(count)").execute.side_effect = Exception("Supabase error")
     response = client.get("/api/v1/companies/companySize")
     
     assert response.status_code == 500
     assert response.json() == {"detail": "Supabase error"}
+
+def test_users_in_company(mock_supabase):
+    mock_company_client = [
+        {"company_id": "test_company_id"}
+    ]
+    mock_users = [
+        {"user_id": "user1", "company_id": "test-company-id"},
+        {"user_id": "user2", "company_id": "test-company-id"},
+    ]
+    mock_supabase.table("company_client").select("company_id").eq("name", "Test Company").execute.return_value.data = mock_company_client
+    mock_supabase.table("users").select("*").eq("company_id", "test_company_id").execute.return_value.data = mock_users
+    response = client.get("/api/v1/companies/Test Company/list")
+    assert response.status_code == 200
+    assert response.json() == {"participants": mock_users}
+
+def test_users_in_company_supabase_error(mock_supabase):
+    mock_supabase.table("company_client").select("*").execute.side_effect = Exception("Supabase error")
+    response = client.get("/api/v1/companies/")
+    
+    assert response.status_code == 500
+    assert response.json() == {"detail": "Supabase error"}
+    
+"""def test_users_in_company_not_found(mock_supabase):
+    mock_supabase.table("company_client").select("company_id").eq("name", "Nonexistent Company").execute.return_value.data = []
+    response = client.get("/api/v1/companies/Nonexistent Company/list")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Company not found"}"""
