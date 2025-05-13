516879d4eb46437c134fd9e8f608d105fac5ed97
Mariana Amy <mmarianaamy@gmail.com>
2025-05-13 09:35:17 -0600

    feat: create company

--- app/api/routes/companies.py
+++ app/api/routes/companies.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from supabase import Client
 
 from app.db.session import get_supabase
 from app.api.routes.auth import check_admin_role
 
 router = APIRouter(prefix="/companies", tags=["companies"])
+
+class Company(BaseModel):
+    name: str
+    logo: str
+    category: str
 
 
 @router.get("/", dependencies=[Depends(check_admin_role)])
@@ -53,5 +59,28 @@ async def get_users_in_company(name: str, supabase: Client = Depends(get_supabas
         )
 
         return {"participants": users_response.data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.post("/create")
+async def get_users_in_company(company: Company, supabase: Client = Depends(get_supabase)):
+    try:
+        responseCategory = supabase.table("category").select("category_id, name").execute()
+
+        if not response.data:
+            raise HTTPException(status_code=404, detail="Category not found")
+        
+        category_id = responseCategory.data[0]["category_id"]
+        company_data = {
+            "name": company.name,
+            "logo": company.logo,
+            "category_id": category_id,
+        }
+
+        response = supabase.table("company_client").insert(company_data).execute()
+        if response.status_code != 201:
+            raise HTTPException(status_code=400, detail="Failed to create company")
+        return {"message": "Company created successfully", "company": response.data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
