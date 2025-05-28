d966fb5eae6dea001a5fd856d825256e853e833f
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2025-05-28 10:25:18 -0600

    feat: chat about specific call

--- app/api/routes/chatbot.py
+++ app/api/routes/chatbot.py
@@ -6,6 +6,7 @@ from app.services.embeddings import (
     chat_with_context,
     suggestions_with_context,
     get_last_embeddings,
+    chat_with_specific_transcript,
 )
 from app.core.config import settings
 import os
@@ -355,5 +356,113 @@ async def get_chat_history(
         return response.data
     except HTTPException:
         raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/continue/specific/{previous_response_id}")
+async def chat_with_specific_call(
+    request: ChatRequest,
+    specific_conversation_id: str,
+    previous_response_id: str,
+    current_user=Depends(get_current_user),
+    supabase: Client = Depends(get_supabase),
+    token_budget: int = 4096 - 500,
+):
+    """
+    Continuar un chat existente
+    """
+    try:
+        if needs_context(request.prompt, previous_response_id):
+            message = await chat_with_specific_transcript(
+                conversation_id=specific_conversation_id,
+                current_user=current_user,
+                supabase=supabase,
+                query=request.prompt,
+                token_budget=token_budget,
+            )
+        else:
+            message = request.prompt
+
+        system_message = "Eres un asistente para un call center. YOU NEVER USE BLOCKS OF CODE OR BLOCK CODES. You may or may not use markdown, but you never use blocks of code regardless"
+        response = await send_to_responses_api(
+            GPT_MODEL, system_message, message, previous_response_id
+        )
+
+        # modify the table to change the last message
+        """ supabase.table("chatbot_conversations").update(
+            {"last_response_id": response.id}
+        ).eq("chatbot_conversation_id", conversation_id).execute()
+
+        await save_messages_to_supabase(
+            conversation_id,
+            request.prompt,
+            response.id,
+            response.output_text,
+            response.created_at,
+            supabase,
+            previous_response_id,
+        ) """
+
+        return {
+            "response": response.output_text,
+            "id": response.id,
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.post("/chat/specific")
+async def post_chat(
+    specific_conversation_id: str,
+    request: ChatRequest,
+    current_user=Depends(get_current_user),
+    supabase: Client = Depends(get_supabase),
+    token_budget: int = 4096 - 500,
+):
+    """Create a new chat"""
+    try:
+        conversation_id = str(uuid4())
+        gpt_model = GPT_MODEL
+        # if the question needs previous context, start a new chat with added embeddings
+        if needs_context(request.prompt):
+            message = await chat_with_specific_transcript(
+                conversation_id=specific_conversation_id,
+                current_user=current_user,
+                supabase=supabase,
+                query=request.prompt,
+                token_budget=token_budget,
+            )
+        else:
+            message = request.prompt
+
+        system_message = "You answer questions for agents of a call center working with multiple companies. YOU NEVER USE BLOCKS OF CODE OR BLOCK CODES. You may or may not use markdown, but you never use blocks of code regardless."
+        response = await send_to_responses_api(gpt_model, system_message, message)
+
+        title = await generate_title(gpt_model, response.id)
+
+        # used for storing in our supabase table
+        """ chatbot_conversation = {
+            "chatbot_conversation_id": conversation_id,
+            "user_id": current_user.id,
+            "title": title.output_text,
+            "last_response_id": response.id,
+            "model": gpt_model,
+        }
+        supabase.table("chatbot_conversations").insert(chatbot_conversation).execute()
+
+        await save_messages_to_supabase(
+            conversation_id,
+            request.prompt,
+            response.id,
+            response.output_text,
+            response.created_at,
+            supabase,
+        ) """
+
+        """ "conversation_id": conversation_id, """
+        return {
+            "response": response.output_text,
+            "title": title.output_text,
+            "id": response.id,
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
